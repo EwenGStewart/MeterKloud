f8f50c59135245b4ff175a525d54c287e8db9d8c
ewen stewart <Ewen.Stewart@tally-group.com>
2024-07-23 19:28:33 +1000

    Add Mud Blazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,12 +2,14 @@ using AzureStaticWebApps.Blazor.Authentication;
 using MeterKloud;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
+builder.Services.AddMudServices();
 
 
 //builder.Services.AddAuthorizationCore();
