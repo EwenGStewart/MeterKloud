6e8bf3d07a88aeaa75c92448d8e64c445c6fde22
ewen stewart <Ewen.Stewart@tally-group.com>
2025-01-08 23:50:50 +1100

    Upgraded Libs

--- Client/Features/Security/MockSecurity.cs
+++ Client/Features/Security/MockSecurity.cs
@@ -1,70 +1,70 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.Extensions.Options;
-using System.Security.Claims;
-using System.Text.Encodings.Web;
+﻿//using Microsoft.AspNetCore.Authentication;
+//using Microsoft.AspNetCore.Components.Authorization;
+//using Microsoft.Extensions.Options;
+//using System.Security.Claims;
+//using System.Text.Encodings.Web;
 
-namespace MeterKloud.Features.Security
-{
-
-
-    public class MockAuthenticatedUser : AuthenticationHandler<AuthenticationSchemeOptions>
-    {
-        private const string userId = "phv";
-        private const string userName = "Jean Irvine";
-        private const string userRole = "ProductManager";
-
-        public MockAuthenticatedUser(
-          IOptionsMonitor<AuthenticationSchemeOptions> options,
-          ILoggerFactory logger,
-          UrlEncoder encoder,
-          ISystemClock clock)
-          : base(options, logger, encoder, clock) { }
+//namespace MeterKloud.Features.Security
+//{
 
 
-        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
-        {
-            var claims = new[]
-              {
-          new Claim(ClaimTypes.NameIdentifier, userId),
-          new Claim(ClaimTypes.Name, userName),
-          new Claim(ClaimTypes.Role, userRole),
-          new Claim(ClaimTypes.Email, "peter.vogel@phvis.com"),
-        };
-            var identity = new ClaimsIdentity(claims, Scheme.Name);
-            var principal = new ClaimsPrincipal(identity);
-            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+//    public class MockAuthenticatedUser : AuthenticationHandler<AuthenticationSchemeOptions>
+//    {
+//        private const string userId = "phv";
+//        private const string userName = "Jean Irvine";
+//        private const string userRole = "ProductManager";
 
-            return await Task.FromResult(AuthenticateResult.Success(ticket));
-        }
-    }
+//        public MockAuthenticatedUser(
+//          IOptionsMonitor<AuthenticationSchemeOptions> options,
+//          ILoggerFactory logger,
+//          UrlEncoder encoder,
+//          ISystemClock clock)
+//          : base(options, logger, encoder, clock) { }
 
 
+//        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+//        {
+//            var claims = new[]
+//              {
+//          new Claim(ClaimTypes.NameIdentifier, userId),
+//          new Claim(ClaimTypes.Name, userName),
+//          new Claim(ClaimTypes.Role, userRole),
+//          new Claim(ClaimTypes.Email, "peter.vogel@phvis.com"),
+//        };
+//            var identity = new ClaimsIdentity(claims, Scheme.Name);
+//            var principal = new ClaimsPrincipal(identity);
+//            var ticket = new AuthenticationTicket(principal, Scheme.Name);
 
-    public class MyServerAuthenticationStateProvider : AuthenticationStateProvider
-    {
-
-        private const string userId = "phv";
-        private const string userName = "Jean Irvine";
-        private const string userRole = "ProductManager";
-
-        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
-        {
-            var claims = new[] {
-          new Claim(ClaimTypes.NameIdentifier, userId),
-          new Claim(ClaimTypes.Name, userName),
-          new Claim(ClaimTypes.Role, userRole),
-          new Claim(ClaimTypes.Email, "peter.vogel@phvis.com"),
-        };
-            var identity = new ClaimsIdentity(claims, "BasicAuthentication");
-            //    var authenticated = true;
-
-            var result =await Task.FromResult( new AuthenticationState(new ClaimsPrincipal(identity)));
-            return result;
-        }
-
-    }
+//            return await Task.FromResult(AuthenticateResult.Success(ticket));
+//        }
+//    }
 
 
-}
+
+//    public class MyServerAuthenticationStateProvider : AuthenticationStateProvider
+//    {
+
+//        private const string userId = "phv";
+//        private const string userName = "Jean Irvine";
+//        private const string userRole = "ProductManager";
+
+//        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+//        {
+//            var claims = new[] {
+//          new Claim(ClaimTypes.NameIdentifier, userId),
+//          new Claim(ClaimTypes.Name, userName),
+//          new Claim(ClaimTypes.Role, userRole),
+//          new Claim(ClaimTypes.Email, "peter.vogel@phvis.com"),
+//        };
+//            var identity = new ClaimsIdentity(claims, "BasicAuthentication");
+//            //    var authenticated = true;
+
+//            var result =await Task.FromResult( new AuthenticationState(new ClaimsPrincipal(identity)));
+//            return result;
+//        }
+
+//    }
+
+
+//}
 
