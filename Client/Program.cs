f3d5bcbc162674d42220d8a06347052503acde78
ewen stewart <ewengstewart@gmail.com>
2026-02-22 20:35:24 +1100

    test

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,10 +8,13 @@ using Microsoft.Extensions.Caching.Memory;
 using MudBlazor.Services;
 
 
+
+
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddMudServices();
 
 
